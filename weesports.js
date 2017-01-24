30dbce5a911810eec2342ddfcb5989fb9cf41cc2
yuyuanhey <cook19960626@gmail.com>
2017-01-24 19:52:30 +0800

    初步概念

---
+++ weesports.js
@@ -0,0 +1,9 @@
+$(document).on("mobileinit", function () {
+  $.mobile.autoInitializePage = false;
+  $.mobile.hashListeningEnabled = false;
+});
+
+function mobileInitPage() {
+  $.mobile.hashListeningEnabled = true;
+  $.mobile.initializePage();
+}
